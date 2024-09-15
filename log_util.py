d91d315764a9422b41fbd910213e3cdeb677da74
robertg55 <robert55gh@gmail.com>
2024-09-15 05:03:03 -0400

    Added log to file functionality

--- log_util.py
+++ log_util.py
@@ -8,8 +8,9 @@ def setup_logger() -> logging.Logger:
     """
     Sets up and configures a logger for the application.
 
-    This function creates a logger that logs messages to `stdout` with a specific format. It sets the log level 
-    to `DEBUG` and ensures that the logger does not add multiple handlers if it has already been set up.
+    This function creates a logger that logs messages to both `stdout` and a log file named with the current
+    date and time in the format 'app_YYYYMMDD_HHMMSS.log'. It sets the log level to `DEBUG` and ensures that
+    the logger does not add multiple handlers if it has already been set up.
 
     :return: A configured logger instance.
     """
@@ -20,16 +21,25 @@ def setup_logger() -> logging.Logger:
     stdout_handler = logging.StreamHandler(sys.stdout)
     stdout_handler.setLevel(logging.DEBUG)
 
+    # Generate a log file name with the current date and time
+    current_time = datetime.now().strftime('%Y%m%d_%H%M%S')
+    log_file = f'app_{current_time}.log'
+
+    # Create a handler for logging to a file
+    file_handler = logging.FileHandler(log_file)
+    file_handler.setLevel(logging.DEBUG)
+
     # Create a formatter
     formatter = logging.Formatter('[%(asctime)s][%(levelname)s]: %(message)s')
     stdout_handler.setFormatter(formatter)
+    file_handler.setFormatter(formatter)
 
     # Avoid adding multiple handlers if the logger is already set up
     if not logger.hasHandlers():
         logger.addHandler(stdout_handler)
+        logger.addHandler(file_handler)
 
     return logger
-
 
 def get_files_in_current_directory() -> list[str]:
     """
